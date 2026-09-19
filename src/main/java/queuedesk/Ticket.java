e829c058527b977cef3f3699b21a7bb97d65c5d6
Anubha0308 <anubhasharma0308@gmail.com>
2026-09-19 23:28:01 +0530

    methods and fields for Ticket class

--- src/main/java/queuedesk/Ticket.java
+++ src/main/java/queuedesk/Ticket.java
@@ -1,12 +1,70 @@
 package queuedesk;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class Ticket {
-    public int id;
-    public String description;
-    public String title;
-    public int requester;
-    public Status status;
-    public Priority priority;
+
+    private static int nextId;
+    private int id;
+    private String description;
+    private String title;
+    private int requester;
+    private Status status;
+    private Priority priority;
+    LocalDateTime createdAt;
+    {
+        nextId=0;
+    }
+    Ticket(String title,String description,String priority,int requester){
+        nextId+=1;
+        this.title=title;
+        this.description=description;
+        this.priority=Priority.valueOf(priority.trim().toUpperCase());;
+        this.requester=requester;
+        this.createdAt=LocalDateTime.now();
+    }
+
+    Ticket(String title,int requester){
+        this(title,"Description","LOW",requester);
+    }
+
+    public int getID(){
+        return this.id;
+    }
+    public String getDescription(){
+        return this.description;
+    }
+    public String getTitle(){
+        return this.title;
+    }
+    public int getRequester(){
+        return this.requester;
+    }
+    public String getStatus(){
+        return this.status;
+    }
+
+    public void setDescription(String description){
+        this.description=description;
+    }
+    public void setPriority(String priority){
+        this.priority=Priority.valueOf(priority.trim().toUpperCase());
+    }
+    public void setStatus(String status){
+        this.status=Status.valueOf(status.trim().toUpperCase());
+    }
+
+    long ageInHours(){
+        LocalDateTime currentTime= LocalDateTime.now();
+        Duration duration= Duration.between(this.createdAt,currentTime);
+        long hours=duration.toHours();
+        return hours;
+    }
+
+    static int totalCreated(){
+        return nextId;
+    }
 
 
 }
